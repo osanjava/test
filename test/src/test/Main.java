7c0d148eb062e2c124b905ce3b3bd85117165ca3
wawenjie <wawenjie@10.1.191.222>
2015-02-28 10:44:51 +0800

    测试

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -7,5 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		String s = "/api/abc";
 		System.out.println(Pattern.matches(s, "/api/abc1234"));
+		System.out.println("不知道！！");
 	}
 }
