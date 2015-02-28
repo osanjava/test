f4e0e658fc893692fe4a5d8b35025d4b86db0fda
wawenjie <wawenjie@10.1.191.222>
2015-02-28 10:24:40 +0800

    111

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		String s = "/api/abc";
-		System.out.println(Pattern.matches(s, "/api/abc"));
+		System.out.println(Pattern.matches(s, "/api/abc1234"));
 	}
 }
