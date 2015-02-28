faebf9fedb5cc53ebe63222ec8af1307c32ec3b0
wawenjie <wawenjie@10.1.191.222>
2015-02-28 12:48:17 +0800

    last

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 public class Main {
 
 	public static void main(String[] args) {
-		String s = "/api/abc";
+		String s = "/api/abc11";
 		System.out.println(Pattern.matches(s, "/api/abc1234"));
-		System.out.println("不知道！！111");
+		System.out.println("不知道！！111222");
 		System.out.println("不知道！！fork---2");
 		System.out.println("不知道！！fork---3");
 	}
